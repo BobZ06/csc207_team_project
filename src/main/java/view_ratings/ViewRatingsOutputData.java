dfb40e823433199012e6c6a42bcae476d7a9621b
BobZ06 <sbob92879@gmail.com>
2025-11-24 18:37:13 -0500

    - Added Input/Output boundaries and data structures - Implemented Interactor for retrieving reviews - Defined Data Access Interface for review retrieval

---
+++ src/main/java/view_ratings/ViewRatingsOutputData.java
@@ -0,0 +1,16 @@
+package view_ratings;
+
+import java.util.List;
+
+public class ViewRatingsOutputData {
+    private final List<String> reviews;
+    private final String restaurantID;
+
+    public ViewRatingsOutputData(List<String> reviews, String restaurantID) {
+        this.reviews = reviews;
+        this.restaurantID = restaurantID;
+    }
+
+    public List<String> getReviews() { return reviews; }
+    public String getRestaurantID() { return restaurantID; }
+}

57926e74e861f7b039e55018d8dda6da6dd9090e
AquaDummy <awesomejay098@gmail.com>
2025-11-29 14:57:44 -0500

    Sorry Mertali. Removed the redundant signup use case.

--- src/main/java/use_case/sign_up/SignupInputBoundary.java
+++
@@ -1,5 +0,0 @@
-package use_case.sign_up;
-
-public interface SignupInputBoundary {
-    void execute(SignupInputData inputData);
-}
--- src/main/java/use_case/sign_up/SignupOutputBoundary.java
+++
@@ -1,6 +0,0 @@
-package use_case.sign_up;
-
-public interface SignupOutputBoundary {
-    void prepareSuccessView(SignupOutputData outputData);
-    void prepareFailView(String error);
-}

3b305b4467574a88b2cf0cc44cf055f8d7fa9731
Vignesh Khajuria <vignesh.khajuria@mail.utoronto.ca>
2025-11-29 18:56:47 -0500

    Added clean ViewMenuInputData

--- src/main/java/use_case/view_menu/ViewMenuInputData.java
+++ src/main/java/use_case/view_menu/ViewMenuInputData.java
@@ -1,4 +1,39 @@
 package use_case.view_menu;
 
+
 public class ViewMenuInputData {
+
+
+    private final String restaurantName;
+    private final String zipCode;
+
+
+    private final String restaurantAddress;
+    private final double restaurantRating;
+
+    public ViewMenuInputData(String restaurantName,
+                             String zipCode,
+                             String restaurantAddress,
+                             double restaurantRating) {
+        this.restaurantName = restaurantName;
+        this.zipCode = zipCode;
+        this.restaurantAddress = restaurantAddress;
+        this.restaurantRating = restaurantRating;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getRestaurantAddress() {
+        return restaurantAddress;
+    }
+
+    public double getRestaurantRating() {
+        return restaurantRating;
+    }
 }
